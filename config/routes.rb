703f78b00be2227f25797a009159aa50274e1a15
takahiro-nmb <89505665+takahiro-nmb@users.noreply.github.com>
2021-11-18 21:41:02 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  
+  devise_for :users
+  get 'prototypes/index'
+  root to: "prototypes#index"
+
+  resources :prototypes do
+    resources :comments, only: [:create, :show]
+  end
+
+  resources :prototypes
+
+  resources :users, only: :show
+end
